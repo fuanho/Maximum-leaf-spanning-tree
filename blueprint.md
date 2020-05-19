c4d78faccf137e2f4ae1aad8e7ac6320298c8d96
Fuan <fuanho@gmail.com>
2020-05-20 00:11:52 +0800

    Input method and initial architecture

---
+++ blueprint.md
@@ -0,0 +1,12 @@
+# Find tree in graph
+
+$$ 
+\sum n-1 
+$$
+
+```mermaid
+    graph TD
+    A---B
+    B---C
+    C---A
+```
--- main.cpp
+++ main.cpp
@@ -1,9 +1,56 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    int a = 1;
-    int b = 2;
-    cout << a+b << endl;
+class graphNode {
+public:
+    int index;
+    vector<int> neighborNode;
+};
+
+class treeNode {
+public:
+    int index;
+    graphNode* upper;
+    graphNode* right;
+    graphNode* left;
+};
+
+vector<graphNode*> buildGraph(vector<int> point) {
+
+}
+
+vector<treeNode*> buildtree(vector<int> point) {
+    
+}
+
+int findTreenumber() {
+
+}
+
+int main() {
+    int nodeSize;
+    vector<int> result;
+    vector<graphNode*> graph;
+    cin >> nodeSize;
+    if(nodeSize != 0) { // Check quantity of nodes
+        for (size_t i = 0; i < nodeSize; i++) { // Input datas
+            graphNode* tmpGraph;
+            tmpGraph->index = i;
+            for (size_t j = 0; j < nodeSize; j++) {
+                int pointNum;
+                cin >> pointNum;
+                if(pointNum == 1) tmpGraph->neighborNode.push_back(j);
+            }
+            graph.push_back(tmpGraph);
+        }
+
+        
+        cin >> nodeSize;
+    }
+
+    for (size_t i = 0; i < result.size(); i++) { // print out results
+        cout << result.at(i) << endl;
+    }
 }
